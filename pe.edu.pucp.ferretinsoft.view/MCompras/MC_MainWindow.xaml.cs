7bb730c2da771ab69e3c9dff710369dcc8ecf30e
croberto <crobertoyl@gmail.com>
2013-09-28 19:17:30 -0500

    Se añadio el submenu de Ordenes de Compra y Proveedores al menu de Compras

--- pe.edu.pucp.ferretinsoft.view/MCompras/MC_MainWindow.xaml.cs
+++ pe.edu.pucp.ferretinsoft.view/MCompras/MC_MainWindow.xaml.cs
@@ -24,10 +24,36 @@ namespace pe.edu.pucp.ferretinsoft.view.MCompras
             InitializeComponent();
         }
 
+        private void homeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.OwnedWindows.Count == 0)
+            {
+                this.Close();
+            }
+        }
+
         private void adminProveedoresBtn_Click(object sender, RoutedEventArgs e)
         {
             MC_AdministrarProveedorWindow MCadmp = new MC_AdministrarProveedorWindow();
-            MCadmp.Show();
+            MCadmp.ShowDialog();
+        }
+
+        private void ordCompraBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MC_AdministrarOCCotizacion MCadoc = new MC_AdministrarOCCotizacion();
+            MCadoc.ShowDialog();
+        }
+
+        private void repComprasBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //ReportesComprasWindow repW = new ReportesComprasWindow();
+            //repW.ShowDialog();
+        }
+
+        private void repComprasBtn_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            //ComprasSolicitudAbastecimiento solAbast = new ComprasSolicitudAbastecimiento();
+            //solAbast.ShowDialog();
         }
     }
 }

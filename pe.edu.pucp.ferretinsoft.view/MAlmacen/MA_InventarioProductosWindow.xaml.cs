b1ebffde2a12e1d90bc41bd05d56bc185ae6367e
CesarGarciaC <cdgarcia@pucp.pe>
2013-10-07 15:28:00 -0500

    Modificación Almacén

--- pe.edu.pucp.ferretinsoft.view/MAlmacen/MA_InventarioProductosWindow.xaml.cs
+++ pe.edu.pucp.ferretinsoft.view/MAlmacen/MA_InventarioProductosWindow.xaml.cs
@@ -23,5 +23,10 @@ namespace pe.edu.pucp.ferretinsoft.view.MAlmacen
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }

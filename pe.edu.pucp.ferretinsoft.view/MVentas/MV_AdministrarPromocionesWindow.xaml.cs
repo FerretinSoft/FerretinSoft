1d83c27c3841326dfb17f5c089296ba713d283c0
lpintov <lpintov@pucp.pe>
2013-10-08 03:12:40 -0500

    TY22

---
+++ pe.edu.pucp.ferretinsoft.view/MVentas/MV_AdministrarPromocionesWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pe.edu.pucp.ferretinsoft.view.MVentas
+{
+    /// <summary>
+    /// Lógica de interacción para MV_AdministrarPromocionesWindow.xaml
+    /// </summary>
+    public partial class MV_AdministrarPromocionesWindow : Window
+    {
+        public MV_AdministrarPromocionesWindow()
+        {
+            InitializeComponent();
+        }
+        private void editarPromocionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            VentasTab.SelectedIndex = 1;
+        }
+    }
+}

0fa1e4326c5c9aeec7d0121127a712555c041d87
jose.quilca <luther1911@msn.com>
2013-10-10 01:13:51 -0500

    Datos en Ventas

--- pe.edu.pucp.ferretinsoft.model/Venta.cs
+++ pe.edu.pucp.ferretinsoft.model/Venta.cs
@@ -9,5 +9,9 @@ namespace pe.edu.pucp.ferretinsoft.model
     public class Venta
     {
         public int Codigo { get; set; }
+        public String FechaVenta { get; set; }
+        public String Importe { get; set; }
+        public Cliente Cliente { get; set; }
+        public Vendedor Vendedor { get; set; }
     }
 }

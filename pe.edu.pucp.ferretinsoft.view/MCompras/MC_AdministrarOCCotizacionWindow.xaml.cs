6c9501affacd3c7e0dfb9bd8b6d0f44806c8e236
croberto <crobertoyl@gmail.com>
2013-10-10 01:04:48 -0500

    Data Ordenes de Compra

--- pe.edu.pucp.ferretinsoft.view/MCompras/MC_AdministrarOCCotizacionWindow.xaml.cs
+++ pe.edu.pucp.ferretinsoft.view/MCompras/MC_AdministrarOCCotizacionWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using pe.edu.pucp.ferretinsoft.model;
 using pe.edu.pucp.ferretinsoft.view.Resources.Custom_Controls;
 
 namespace pe.edu.pucp.ferretinsoft.view.MCompras
@@ -20,9 +21,12 @@ namespace pe.edu.pucp.ferretinsoft.view.MCompras
     /// </summary>
     public partial class MC_AdministrarOCCotizacion : Window
     {
+        public static List<OrdenCompra> ordenesCompra = ListOrdenCompra();
+
         public MC_AdministrarOCCotizacion()
         {
             InitializeComponent();
+            ordenesDgv.ItemsSource = ordenesCompra;
         }
 
         
@@ -89,6 +93,58 @@ namespace pe.edu.pucp.ferretinsoft.view.MCompras
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private static List<OrdenCompra> ListOrdenCompra()
+        {
+            List<OrdenCompra> ordenesCompra = new List<OrdenCompra>();
+
+            OrdenCompra oc1 = new OrdenCompra();
+            oc1.codigo = "OC001";
+            oc1.proveedor = MC_AdministrarProveedorWindow.proveedores[0];
+            oc1.fecEmision = "04/10/2013";
+            oc1.fecPago = "12/10/2013";
+            oc1.aprobadoPor = "Juan Perez";
+            oc1.emitidoPor = "Carolina Suarez";
+            oc1.ultimaCompra = "15/05/2013";
+            oc1.subTotal = 1500.50;
+            oc1.igv = 285.10;
+            oc1.total = 1785.6;
+            oc1.estado = "Con Factura";
+
+            ordenesCompra.Add(oc1);
+
+            return ordenesCompra;
+        }
+
+        public void codOrden_Click(object sender, RoutedEventArgs e)
+        {
+            var rowData = ((Hyperlink)e.OriginalSource).DataContext as OrdenCompra;
+
+            codOrdenTxtBox.Text = rowData.codigo;
+            proveedorTxtBox.Text = rowData.proveedor.razSoc;
+            fecEmiDp.Text = rowData.fecEmision;
+            fecPagoDp.Text = rowData.fecPago;
+            ingresadoPorTxtBox.Text = rowData.emitidoPor;
+            aprobadoPorTxtBox.Text = rowData.aprobadoPor;
+            razSoc_Lbl.Content = rowData.proveedor.razSoc;
+            direcLbl.Content = rowData.proveedor.domicilio;
+            contactoLbl.Content = rowData.proveedor.nombContacto;
+            contactoTlfLbl.Content = rowData.proveedor.tlfContacto;
+            ultCompraTxtBox.Text = rowData.ultimaCompra;
+            subTotalTxtBox.Text = rowData.subTotal.ToString();
+            igvTxtBox.Text = rowData.igv.ToString();
+            totalTxtBox.Text = rowData.total.ToString();
+
+            ordenesCompraTab.SelectedIndex = 1;
+
+            if (rowData.estado.Equals("Con Factura"))
+            {
+                codOrdenTxtBox.IsEnabled = false;
+                proveedorTxtBox.IsEnabled = false;
+                fecEmiDp.IsEnabled = false;
+                fecPagoDp.IsEnabled = false;
+            }
         }
     }
 }

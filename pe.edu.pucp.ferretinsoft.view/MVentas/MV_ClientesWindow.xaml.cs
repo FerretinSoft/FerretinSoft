30844d07f3683c99a302224cc0fd80aa3297010f
lpintov <lpintov@pucp.pe>
2013-10-10 01:23:24 -0500

    CLUpdate2

--- pe.edu.pucp.ferretinsoft.view/MVentas/MV_ClientesWindow.xaml.cs
+++ pe.edu.pucp.ferretinsoft.view/MVentas/MV_ClientesWindow.xaml.cs
@@ -35,35 +35,85 @@ namespace pe.edu.pucp.ferretinsoft.view.MVentas
             {
                
                     Codigo = 000000526358,
-                    Nombre = "Simón Bolivar",
+                    Nombre = "Simón",
+                    Apellido ="Bolivar",
                     TipoDocumento = "RUC",
                     TipoCliente = "Jurídico",
                     TelefonoCliente = "5550555",
                     EmailCliente = "simon.elInmortal@gmail.com",
-                    PuntosCliente = 100
+                    PuntosCliente = 100,
+                    PuntosGanadosCliente = 1000,
+                    PuntosUsadosCliente = 900,
+                    UltimaCompraCliente = "15/09/2013",
+                    TotalComprasCliente = 5,
+                    RegistradoCliente = "Josefina Alfaro",
+                    DireccionCliente = "Avenida Los Libertadores Nro 200 Int. 405"
             });
             clientes.Add(new Cliente
             {
                     Codigo = 0000008569524,
-                    Nombre = "Daniel Alcides Carrión",
-                    TipoDocumento = "DNI",
+                    Nombre = "Daniel",
+                    Apellido = "Alcides Carrión",
+                    TipoDocumento = "RUC",
                     TipoCliente = "Jurídico",
                     TelefonoCliente = "1020304",
                     EmailCliente = "daniel.elSanador@gmail.com",
-                    PuntosCliente = 500
+                    PuntosCliente = 500,
+                    PuntosGanadosCliente = 5000,
+                    PuntosUsadosCliente = 4500,
+                    UltimaCompraCliente = "01/09/2013",
+                    TotalComprasCliente = 20,
+                    RegistradoCliente = "Josefina Alfaro",
+                    DireccionCliente = "Av. Los Curadores 3era Etapa"
             });
             clientes.Add(new Cliente
             {     
                     Codigo = 70456862,
-                    Nombre = "Ignacio de Loyola",
-                    TipoDocumento = "RUC",
+                    Nombre = "Ignacio",
+                    Apellido = "De Loyola",
+                    TipoDocumento = "DNI",
                     TipoCliente = "Natural",
                     TelefonoCliente = "4240745",
                     EmailCliente = "ignacio.elBueno@gmail.com",
-                    PuntosCliente = 1800
+                    PuntosCliente = 1800,
+                    PuntosGanadosCliente = 5000,
+                    PuntosUsadosCliente = 3200,
+                    UltimaCompraCliente = "10/09/2013",
+                    TotalComprasCliente = 10,
+                    RegistradoCliente = "Josefina Alfaro",
+                    DireccionCliente = "Calle El Mas Bueno 515 Dpto 202"
             });
 
             return clientes;
+        }
+
+        public void numDocumento_Click(object sender, RoutedEventArgs e)
+        {
+
+            var rowData = ((Hyperlink)e.OriginalSource).DataContext as Cliente;
+
+            numDocTxtBox.Text = (rowData.Codigo)+"";
+            nombreClienteTxtBox.Text = rowData.Nombre;
+            if (rowData.TipoDocumento == "DNI")
+                documentoCombo.SelectedIndex = 0;
+            else
+                documentoCombo.SelectedIndex = 1;
+            if (rowData.TipoCliente == "Natural")
+                tipoClienteCombo.SelectedIndex = 0;
+            else
+                tipoClienteCombo.SelectedIndex = 1;
+
+            apellidoClienteTxtBox.Text = rowData.Apellido;
+            telefonoClienteTxtBox.Text = rowData.TelefonoCliente;
+            emailClienteTxtBox.Text = rowData.EmailCliente;
+            puntosActualClienteTxtBox.Text = rowData.PuntosCliente+"";
+             puntosGanadosClienteTxtBox.Text = rowData.PuntosGanadosCliente+"";
+             puntosUsadosClienteTxtBox.Text = rowData.PuntosUsadosCliente+"";
+             ultCompraClienteTxtBox.Text = rowData.UltimaCompraCliente;
+             totalComprasClienteTxtBox.Text=rowData.TotalComprasCliente+"";
+             vendedorClienteTxtBox.Text=rowData.RegistradoCliente;
+             direccionClienteTxtBox.Text=rowData.DireccionCliente;
+            clientesTab.SelectedIndex = 1;
         }
 
         public MV_ClientesWindow(MV_AdministrarVentasWindow MV_AdministrarVentasWindow)

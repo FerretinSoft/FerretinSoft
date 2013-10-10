cbb2306e52e19096b3815f8e1a4593a9317578ff
LuisEspinoza212 <l.espinoza212@gmail.com>
2013-10-10 00:02:27 -0500

    Se "jarcodeo" datos de perfiles

---
+++ pe.edu.pucp.ferretinsoft.model/Perfil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pe.edu.pucp.ferretinsoft.model
+{
+    public class Perfil
+    {
+        public String perf{ get;  set; }
+        public String Modulo { get; set; }
+        public String Descripcion { get; set;}
+    }
+}

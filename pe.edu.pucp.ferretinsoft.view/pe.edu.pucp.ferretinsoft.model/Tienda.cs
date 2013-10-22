7fd5c0deb083003bfaf0071eb2b4935555eb7b0e
mmc01 <a20097102@pucp.edu.pe>
2013-10-22 02:55:26 -0500

    cambios

--- pe.edu.pucp.ferretinsoft.view/pe.edu.pucp.ferretinsoft.model/Tienda.cs
+++ pe.edu.pucp.ferretinsoft.view/pe.edu.pucp.ferretinsoft.model/Tienda.cs
@@ -8,9 +8,21 @@ namespace pe.edu.pucp.ferretinsoft.model
 {
     public class Tienda
     {
-        public String codigo { get; set; }
+        public int codigo { get; set; }
         public String nombre { get; set; }
+        public String tipo { get; set; }
+        public int codigoAlmacenAbastecedor { get; set; }        
         public String direccion { get; set; }
+        public String distrito { get; set; }
+        public String codigoPostal { get; set; }
+        public String provincia { get; set; }
+        public String pais { get; set; }
+        public String telefono1 { get; set; }
+        public String telefono2 { get; set; }
+        public String idUbigeo { get; set; }
+        public String descripcion { get; set; }
+        public String fechaCreacion { get; set; }
+        public Cliente idCliente { get; set; }
 
     }
 }

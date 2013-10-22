747eddf923daf3a07209b5e4e87841930d45bd96
lpintov <lpintov@pucp.pe>
2013-10-22 00:06:56 -0500

    VentasPrueba

---
+++ pe.edu.pucp.ferretinsoft.view/pe.edu.pucp.ferretinsoft.controller/Services/ClienteService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IBatisNet.DataMapper;
+using pe.edu.pucp.ferretinsoft.model;
+
+namespace Project_FerretinSoft.pe.edu.pucp.ferretinsoft.controller.Services
+{
+    class ClienteService
+    {
+        public static List<Cliente> obtenerListaClientes()
+        {
+            List<Cliente> listaClientes = Mapper.Instance().QueryForList("obtenerListaClientes", null) as List<Cliente>;
+            return listaClientes;
+
+        }
+    }
+}

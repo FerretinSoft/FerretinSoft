ef895ff46c76696f425d8091e84dfc330ea5e558
heidyhb <heidyhb89@gmail.com>
2013-10-23 21:03:06 -0500

    Clases añadidas al modelo Movimiento, TipoMovimiento y EstadoMovimiento

---
+++ pe.edu.pucp.ferretinsoft.view/pe.edu.pucp.ferretinsoft.model/Movimiento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_FerretinSoft.pe.edu.pucp.ferretinsoft.model
+{
+    class Movimiento
+    {
+        public DateTime Fecha { get; set; }
+
+        public String Comentario { get; set; }
+
+        public TipoMovimiento tipoMovimiento { get; set; }
+
+        public EstadoMovimiento estado { get; set; }
+    }
+}

b0768d781a31a37c966fbe6c56a3edba26909f42
CesarGarciaC <cdgarcia@pucp.pe>
2013-10-23 21:19:41 -0500

    ?

--- pe.edu.pucp.ferretinsoft.view/pe.edu.pucp.ferretinsoft.controller/Services/CategoriasService.cs
+++ pe.edu.pucp.ferretinsoft.view/pe.edu.pucp.ferretinsoft.controller/Services/CategoriasService.cs
@@ -10,6 +10,10 @@ namespace pe.edu.pucp.ferretinsoft.controller.Services
 {
     class CategoriasService
     {
+        /**************************************************
+         * ->Permite obtener una lista con todas las categorias 
+         * de productos.
+         * ************************************************/
         public static IList<Categorias> obtenerCategorias()
         {
             IList<Categorias> listaCat = Mapper.Instance().QueryForList<Categorias>("obtenerListaCategorías", null);
@@ -17,6 +21,26 @@ namespace pe.edu.pucp.ferretinsoft.controller.Services
         }
 
 
+        /**************************************************
+         * ->Permite obtener una lista con todas las categorias 
+         * de un producto dado.
+         * Input: 
+         * -idProd:Id de prod. del cual se quiere obtener las categorías
+         * Output:
+         * -Lista con todos las categorías que posee el prod.
+         * ************************************************/
+        public static IList<String> obtenerCategoríasPorProducto(int idProd)
+        {
+            IList<int> listaCat = Mapper.Instance().QueryForList<int>("obtenerCategoríasPorProducto", idProd);
+
+            IList<String> nombreCat=new List<String>();
+            foreach (int idCat in listaCat)
+            {
+                nombreCat.Add(Mapper.Instance().QueryForObject<String>("obtenerNombreCategoría", idCat));
+            }
+            
+            return nombreCat;
+        }
 
 
     }
